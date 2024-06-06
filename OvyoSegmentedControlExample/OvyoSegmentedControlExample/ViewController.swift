a65d2a724e9d8170ba6b481c314f3bf48c40e151
parveen-khan-ovyo <parveen.khan@ovyo.com>
2024-06-06 12:38:41 +0530

    implemented tab action in example
    implemented tab action in example

--- OvyoSegmentedControlExample/OvyoSegmentedControlExample/ViewController.swift
+++ OvyoSegmentedControlExample/OvyoSegmentedControlExample/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import OvyoSegmentedControlPackage
 import SwiftUI
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SegmentedControlDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,17 +23,33 @@ class ViewController: UIViewController {
         viewInterface.setFontName("HelveticaNeue-Bold")
         viewInterface.setFontSize(20)
         viewInterface.addTarget(self)
-       // viewInterface.setItemBackgroundColor(.red)
-       // viewInterface.setItemSelectedBackgroundColor(.green)
-       // viewInterface.setCornerRadius(20)
+        // viewInterface.setItemBackgroundColor(.red)
+        // viewInterface.setItemSelectedBackgroundColor(.green)
+        // viewInterface.setCornerRadius(20)
         
         let view1: UIView = viewInterface.create()
         view.addSubview(view1)
-
-        view1.frame = CGRect(x: 0, y: 50, width: view.bounds.width, height: 40)
         
+        view1.frame = CGRect(x: 0, y: 50, width: view.bounds.width, height: 40)
     }
 
+    func tabDidClickAt(_ index: Int) {
+        
+        switch index {
+        case 0:
+            self.view.backgroundColor = .red
+        case 1:
+            self.view.backgroundColor = .gray
+        case 2:
+            self.view.backgroundColor = .cyan
+        case 4:
+            self.view.backgroundColor = .purple
+        case 3:
+            self.view.backgroundColor = .systemPink
+        default:
+            self.view.backgroundColor = .white
+        }
+    }
 
 }
 
