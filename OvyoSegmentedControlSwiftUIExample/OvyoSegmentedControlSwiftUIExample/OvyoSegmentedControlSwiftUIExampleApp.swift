bdb40cf10768d86733dfa84cd6fd17005d8ba33f
parveen-khan-ovyo <parveen.khan@ovyo.com>
2024-06-11 19:51:49 +0530

    Integeration of OvyoSegmentedControl with SwiftUI

---
+++ OvyoSegmentedControlSwiftUIExample/OvyoSegmentedControlSwiftUIExample/OvyoSegmentedControlSwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  OvyoSegmentedControlSwiftUIExampleApp.swift
+//  OvyoSegmentedControlSwiftUIExample
+//
+//  Created by Parveen Khan on 11/06/24.
+//
+
+import SwiftUI
+
+@main
+struct OvyoSegmentedControlSwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
